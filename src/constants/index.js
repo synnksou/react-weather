2bd724cc4ac6ac42fb8f3faef98a0f20f649ebf4
Leplan Ludwig <leplan.ludwig@gmail.com>
2022-10-28 15:06:02 +0200

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,3 @@
+// Good but you should use an .env file instead of js file for the values and dont treat them in public
 export const API_KEY = 'ba9cb7c0c99f38cf9dd60ab44b04e814';
 export const API_BASE = 'https://api.openweathermap.org/data/2.5/';
